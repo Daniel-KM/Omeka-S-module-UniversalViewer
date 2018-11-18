38bbce1ea88692c602fda66daa8ea3c39ced4969
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-11-19 00:00:00 +0100

    Updated gulpfile to fix automatically the iframe charset inside Omeka.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,55 @@
 'use strict';
 
-var async = require('async');
-var del = require('del');
-var fs = require('fs');
-var glob = require("glob");
-var gulp = require('gulp');
-var rename = require("gulp-rename");
+const gulp = require('gulp');
+const del = require('del');
+const fs = require('fs');
+const glob = require("glob");
+const rename = require("gulp-rename");
+const replace = require('gulp-string-replace');
+
+const bundle = [
+    {
+        'source': 'node_modules/universalviewer/dist/uv-*/**',
+        'dest': 'asset/vendor/uv',
+    },
+];
 
 gulp.task('clean', function(done) {
-    return del('asset/vendor/uv');
-});
-
-gulp.task('sync', function(done) {
-    async.series([
-        function (next) {
-            gulp.src(['node_modules/universalviewer/dist/uv-*/**'])
-            .pipe(gulp.dest('asset/vendor/'))
-            .on('end', next);
-        }
-    ], done);
-});
-
-gulp.task('rename', function(done) {
-    var file = glob.sync('asset/vendor/uv-*/');
-    fs.renameSync(file[0], 'asset/vendor/uv/');
+    bundle.forEach(function (module) {
+        return del.sync(module.dest);
+    });
     done();
 });
 
-gulp.task('default', gulp.series('clean', 'sync', 'rename'));
+gulp.task('sync', function (done) {
+    bundle.forEach(function (module) {
+        gulp.src(module.source)
+            .pipe(gulp.dest(module.dest))
+            .on('end', done);
+    });
+});
+
+// The dist is unknown, so it should be renamed.
+const rename_uv = function(done) {
+    var file = glob.sync('asset/vendor/uv/uv-*/');
+    fs.renameSync(file[0], 'asset/vendor/uv_dist/');
+    del.sync('asset/vendor/uv/');
+    fs.renameSync('asset/vendor/uv_dist/', 'asset/vendor/uv/');
+    done();
+};
+
+// Avoid a warning for unknown charset in a iframe.
+const hack_uv = function (done) {
+    gulp.src(['asset/vendor/uv/app.html'])
+        .pipe(replace(
+            /^<head>$/gm,
+            '<head>' + "\n" + '    <meta charset="UTF-8">'
+        ))
+        .pipe(gulp.dest('asset/vendor/uv/'))
+        .on('end', done);
+};
+
+gulp.task('default', gulp.series('clean', 'sync', rename_uv, hack_uv));
 
 gulp.task('install', gulp.task('default'));
 
