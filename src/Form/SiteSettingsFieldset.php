1c704a6ac253ecd4e1c1495d5c85b5bad869b8b3
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Used Omeka S v4 feature "element group" for settings.

--- src/Form/SiteSettingsFieldset.php
+++ src/Form/SiteSettingsFieldset.php
@@ -9,14 +9,24 @@ class SiteSettingsFieldset extends Fieldset
 {
     protected $label = 'Universal Viewer'; // @translate
 
+    protected $elementGroups = [
+        // "Player" is used instead of viewer, because "viewer" is used for a site
+        // user role and cannot be translated differently (no context).
+        // Player is polysemic too anyway, but less used and more adapted for
+        // non-image viewers.
+        'player' => 'Players', // @translate
+    ];
+
     public function init(): void
     {
         $this
             ->setAttribute('id', 'universal-viewer')
+            ->setOption('element_groups', $this->elementGroups)
             ->add([
                 'name' => 'universalviewer_version',
                 'type' => Element\Radio::class,
                 'options' => [
+                    'element_group' => 'player',
                     'label' => 'Version of Universal viewer', // @translate
                     'value_options' => [
                         '2' => 'Version 2.0.2 (better speed for some scanned pdf; require iiif v2)', // @translate
