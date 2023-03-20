9e46bf711eaca418aa7fbae608d13aa621db899c
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-03-20 00:00:00 +0000

    Cleaned code.

--- src/Form/SiteSettingsFieldset.php
+++ src/Form/SiteSettingsFieldset.php
@@ -30,7 +30,7 @@ class SiteSettingsFieldset extends Fieldset
                     'label' => 'Version of Universal viewer', // @translate
                     'value_options' => [
                         '2' => 'Version 2.0.2 (better speed for some scanned pdf; require iiif v2)', // @translate
-                        '3' => 'Version 3.1.1 (more modern)', // @translate
+                        '3' => 'Version 3.1.1 (more modern, but deprecated)', // @translate
                         '4' => 'Version 4 (up to date)', // @translate
                     ],
                 ],
