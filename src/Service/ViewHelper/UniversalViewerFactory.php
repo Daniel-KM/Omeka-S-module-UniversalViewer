e75f564302ed62daa851f3d693e69cd5d3df02fb
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-03-02 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/ViewHelper/UniversalViewerFactory.php
+++ src/Service/ViewHelper/UniversalViewerFactory.php
@@ -2,7 +2,7 @@
 
 namespace UniversalViewer\Service\ViewHelper;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use UniversalViewer\View\Helper\UniversalViewer;
 
